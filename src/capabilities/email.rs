941b8c6c48596aab4a5fae4546e9af5c019129aa
Brian Ginsburg <7957636+bgins@users.noreply.github.com>
2023-07-31 10:42:14 -0700

    feat: Add email and wnfs capabilities (#4)

---
+++ src/capabilities/email.rs
@@ -0,0 +1,64 @@
+use anyhow::{anyhow, Result};
+use ucan::capability::{Ability, CapabilitySemantics, Scope};
+use url::Url;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct EmailAddress(String);
+
+impl Scope for EmailAddress {
+    fn contains(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl ToString for EmailAddress {
+    fn to_string(&self) -> String {
+        format!("mailto:{}", self.0.clone())
+    }
+}
+
+impl TryFrom<Url> for EmailAddress {
+    type Error = anyhow::Error;
+
+    fn try_from(value: Url) -> Result<Self> {
+        match value.scheme() {
+            "mailto" => Ok(EmailAddress(String::from(value.path()))),
+            _ => Err(anyhow!(
+                "Could not interpret URI as an email address: {}",
+                value
+            )),
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Debug)]
+pub enum EmailAction {
+    Send,
+}
+
+impl Ability for EmailAction {}
+
+impl ToString for EmailAction {
+    fn to_string(&self) -> String {
+        match self {
+            EmailAction::Send => "email/send",
+        }
+        .into()
+    }
+}
+
+impl TryFrom<String> for EmailAction {
+    type Error = anyhow::Error;
+
+    fn try_from(value: String) -> Result<Self> {
+        match value.as_str() {
+            "email/send" => Ok(EmailAction::Send),
+            _ => Err(anyhow!("Unrecognized action: {}", value)),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct EmailSemantics {}
+
+impl CapabilitySemantics<EmailAddress, EmailAction> for EmailSemantics {}
