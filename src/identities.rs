49af04d57144f017951298d2e92c48f47f5ff35b
Brian Ginsburg <gins@brianginsburg.com>
2023-08-16 11:33:31 -0700

    Expose Alice's base64 key

--- src/identities.rs
+++ src/identities.rs
@@ -16,9 +16,12 @@ where
     pub mallory_did: String,
 }
 
+pub const ALICE_BASE64_KEY: &str =
+    "U+bzp2GaFQHso587iSFWPSeCzbSfn/CbNHEz7ilKRZ1UQMmMS7qq4UhTzKn3X9Nj/4xgrwa+UqhMOeo4Ki8JUw==";
+
 impl Identities<Ed25519KeyMaterial> {
     pub async fn new() -> Self {
-        let alice_key = ed25519_key_from_base64("U+bzp2GaFQHso587iSFWPSeCzbSfn/CbNHEz7ilKRZ1UQMmMS7qq4UhTzKn3X9Nj/4xgrwa+UqhMOeo4Ki8JUw==").unwrap();
+        let alice_key = ed25519_key_from_base64(ALICE_BASE64_KEY).unwrap();
         let bob_key  = ed25519_key_from_base64("G4+QCX1b3a45IzQsQd4gFMMe0UB1UOx9bCsh8uOiKLER69eAvVXvc8P2yc4Iig42Bv7JD2zJxhyFALyTKBHipg==").unwrap();
         let mallory_key  = ed25519_key_from_base64("LR9AL2MYkMARuvmV3MJV8sKvbSOdBtpggFCW8K62oZDR6UViSXdSV/dDcD8S9xVjS61vh62JITx7qmLgfQUSZQ==").unwrap();
 
